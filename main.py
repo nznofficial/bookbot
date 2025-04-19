d7b04e0f356bd423fce58f40685f878c71e86db0
Emmanuel Vazquez <emvdev@Emmanuels-MacBook-Air.local>
2025-04-19 01:50:16 -0700

    trippy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, count_chars
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -11,3 +11,6 @@ frank_results = get_book_text(frank)
 
 word_count = get_num_words(frank_results)
 print(f"{word_count} words found in the document")
+
+char_count = count_chars(frank_results)
+print(char_count)
