54795444954c8525da835f95f403b173664b1212
Emmanuel Vazquez <emvdev@Emmanuels-MacBook-Air.local>
2025-04-27 21:07:11 -0700

    PrintBot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, count_chars
+from stats import get_num_words, count_chars, sort_on, char_sort
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -13,4 +13,8 @@ word_count = get_num_words(frank_results)
 print(f"{word_count} words found in the document")
 
 char_count = count_chars(frank_results)
-print(char_count)
+
+sorted_char = char_sort(char_count)
+final_sort = sort_on(sorted_char)
+
+print(final_sort)
