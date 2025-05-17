ec7386db86b999dd4294ec6e2c11d2c2ec0b6c52
Emmanuel Vazquez <emvdev@Emmanuels-MacBook-Air.local>
2025-05-16 23:24:08 -0700

    all

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from stats import get_num_words, count_chars, char_sort
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python3 main.py <path to book>")
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     book_path = sys.argv[1]
 
