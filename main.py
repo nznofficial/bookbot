cbf77759c5bdb5a606c0f09b43dcafbf7df60baa
Emmanuel Vazquez <emvdev@Emmanuels-MacBook-Air.local>
2025-05-16 23:17:45 -0700

    all

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
+import sys
 from stats import get_num_words, count_chars, char_sort
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path to book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = count_chars(text)
+    chars_sorted_list = char_sort(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-frank = "books/frankenstein.txt"
 
-frank_results = get_book_text(frank)
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------") 
+    print(f"Found {num_words} total words")
 
-print("============ BOOKBOT ============")
-print(f"Analyzing book found at {frank}...")
-print("----------- Word Count ----------") 
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
 
-word_count = get_num_words(frank_results)
-print(f"Found {word_count} total words")
+    print("============= END ==============")
 
-print("--------- Character Count -------") 
-char_count = count_chars(frank_results)
-
-sorted_char = char_sort(char_count)
-
-for item in sorted_char:
-    if item["char"].isalpha():
-        print(f"{item['char']}: {item['num']}")
-
-print("============= END ==============")
+main()
